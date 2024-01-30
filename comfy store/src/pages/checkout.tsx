d2d8613d0a1f807793b2cb28e3920c227481cd98
josephchimebuka <chimebujoseph@gmail.com>
2024-01-30 19:59:40 +0000

    added cartitems in checkout

--- comfy store/src/pages/checkout.tsx
+++ comfy store/src/pages/checkout.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
+import CartTotal from '../components/CartTotal'
+import CheckoutForm from '../components/CheckoutForm'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../features/cart/root-reducer'
+import { SectionTitle } from '../components'
 const checkout = () => {
+  const cartItem =  useSelector((state:RootState) => (state.cartreducer.cartTotal))
+  if(cartItem === 0){
+    <SectionTitle text='There is no product in the cart'/>
+  }
   return (
     <div>
-      
+      <SectionTitle text={'Place your order'}/>
+      <CheckoutForm/>
+      <CartTotal/>
     </div>
   )
 }
