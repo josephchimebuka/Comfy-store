dc97743071689e80dfd1c9aa4a3b4abd1882a5b5
josephchimebuka <chimebujoseph@gmail.com>
2024-02-06 18:57:51 +0000

    adjused header component

--- comfy store/src/components/HeaderComponent.tsx
+++ comfy store/src/components/HeaderComponent.tsx
@@ -20,7 +20,7 @@ const HeaderComponent = () => {
              <div className='align-element flex justify-center sm:justify-end '>
              <div className='align-element flex justify-center sm:justify-end '>
         {user ? (
-          <div className='flex gap-x-2 sm:gap-x-8 items-center'>
+          <div className='flex gap-x-2 sm:gap-x-6 items-center'>
             <p className='text-xs sm:text-sm'>Hello, {user.username}</p>
             <button
               className='btn btn-xs btn-outline btn-primary '
